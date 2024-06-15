1a098c15bbcc3bdce6ae72f175bb79c3fd2442a0
yxc0731 <77831135+yxc0731@users.noreply.github.com>
2024-06-15 19:36:09 +0800

    Update clip_attack.py

--- clip_attack.py
+++ clip_attack.py
@@ -4,51 +4,69 @@ import torch
 from torchvision.transforms.functional import to_tensor, to_pil_image
 import pandas as pd
 
+def normalize(images):
+    mean = torch.tensor([0.48145466, 0.4578275, 0.40821073]).to(device)
+    std = torch.tensor([0.26862954, 0.26130258, 0.27577711]).to(device)
+    images = images - mean[None, :, None, None]
+    images = images / std[None, :, None, None]
+    return images
+
+def denormalize(images):
+    mean = torch.tensor([0.48145466, 0.4578275, 0.40821073]).to(device)
+    std = torch.tensor([0.26862954, 0.26130258, 0.27577711]).to(device)
+    images = images * std[None, :, None, None]
+    images = images + mean[None, :, None, None]
+    return images
+    
 df = pd.read_csv('attack_dataset.csv')
+
 model = CLIPModel.from_pretrained(
     "openai/clip-vit-large-patch14")
 processor = CLIPProcessor.from_pretrained(
     "openai/clip-vit-large-patch14")
 
-
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = model.to(device)
 
+epsilon = 8/255  
+alpha = 1/255
 
-def optimize_image(img_harm_path, img_adv_path, learning_rate=0.01, num_iterations=1000, output_path="adv_image.png"):
-
+def optimize_image_with_noise(img_harm_path, img_adv_path, num_iterations=1000,
+                              output_path="final_adv_image.png"):
     image_harm = Image.open(img_harm_path).convert("RGB")
     image_adv = Image.open(img_adv_path).convert("RGB")
-
-
     inputs_harm = processor(images=image_harm, return_tensors="pt").to(device)
-    inputs_adv = processor(images=image_adv, return_tensors="pt").to(device)
+    x = processor(images=image_adv, return_tensors="pt").pixel_values.to(device)
 
+    adv_noise = torch.rand_like(x).to(device) * 2 * epsilon - epsilon
+    x = denormalize(x).clone().to(device)
+    adv_noise.data = (adv_noise.data + x.data).clamp(0, 1) - x.data
+    adv_noise.requires_grad_(True)
+    adv_noise.retain_grad()
 
     H_harm = model.get_image_features(**inputs_harm).detach()
-
-
-    x_adv = inputs_adv.pixel_values.requires_grad_(True)
-
-
-    optimizer = torch.optim.Adam([x_adv], lr=learning_rate)
-
-
+                                  
     for iteration in range(num_iterations):
-        print(iteration)
-        optimizer.zero_grad()
-        H_adv = model.get_image_features(pixel_values=x_adv)
+        adv_img = normalize(x + adv_noise)
+        H_adv = model.get_image_features(pixel_values=adv_img)
         loss = torch.norm(H_adv - H_harm, p=2)
-        print(loss)
         loss.backward()
-        optimizer.step()
 
+        adv_noise.data = (adv_noise.data - alpha * adv_noise.grad.detach().sign()).clamp(-epsilon, epsilon)
+        adv_noise.data = (adv_noise.data + x.data).clamp(0, 1) - x.data
+        adv_noise.grad.zero_()
+        if iteration % 100 == 0:
+            print(iteration)
+            print(loss)
         if loss.item() < 0.3:
             break
 
+        if iteration % 1000 == 0:
+            iter_image = to_pil_image((x + adv_noise).clamp(0, 1).detach().cpu().squeeze(0))
+            iter_image.save(os.path.join(output_folder, f"adv_image_{iteration}.png"))
 
-    final_adv_image = to_pil_image(torch.clamp(x_adv.detach().cpu().squeeze(0), 0, 1))
 
+    final_adv_image = to_pil_image((x + adv_noise).clamp(0, 1).detach().cpu().squeeze(0))
     final_adv_image.save(output_path)
 
     return output_path
